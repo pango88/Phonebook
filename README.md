403caaf7165abd647f72525a387f3ed8d6cbf00f
pango88 <hansflame@protonmail.com>
2020-04-17 00:26:43 +0200

    hopefully final push, tried to fix so that both folders can open

---
+++ README.md
@@ -0,0 +1,4 @@
+# Phonebook
+This project is a part of the course fullstackopenrepo. I built the frontend with react and create-react-app, and the backend is built with node.js and express, the database used in this project is mongodb and it is stored on the mongodb atlas cloud.
+
+here is a link to the actual project already hosted on heroku: https://boiling-reef-31870.herokuapp.com/
---
+++ phonebook_backend/build/precache-manifest.0e11fe35d46cf3cfb52fa08f4cff9045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d995522e55f151e017d1f03f4a9b388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c86641548e8f7a15e0e",
+    "url": "/static/css/main.d2910d7f.chunk.css"
+  },
+  {
+    "revision": "6942fab7ef32df5f1874",
+    "url": "/static/js/2.0fd52afe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0fd52afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c86641548e8f7a15e0e",
+    "url": "/static/js/main.72f727b8.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
